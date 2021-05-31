b7f1e29a793f841dc6cc135a01a4bfe38daef63d
Max Blamauer <maxblamauer@gmail.com>
2021-05-30 21:52:15 -0400

    react-site start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Card from "./components/Card";
+import stevie from "./images/stevieDock.png";
+import stevie2 from "./images/stevieDock2.png";
+import stevie3 from "./images/stevieDock3.png";
+import stevie4 from "./images/stevieDock4.png";
+import walking from "./images/walking.png";
+import stevieRock from "./images/stevieRock.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Card
+        title="Calm and relaxed"
+        imageUrl={stevie}
+        body="Stevie at a cottage on Chandos lake in North Kwartha Photo taken by: @dallasblissphotography"
+      />
+      <Card
+        title="Cool and collected"
+        imageUrl={stevie2}
+        body="Stevie staring intently at something off in the distance. Photo taken by: @dallasblissphotography"
+      />
+      <Card
+        title="Chasing birds"
+        imageUrl={stevie3}
+        body="Stevie guarding the cottage from predators Photo taken by: @dallasblissphotography"
+      />
+      <Card
+        title="Wild goose chase"
+        imageUrl={stevie4}
+        body="Stevie once again guarding the cottage from animals Photo taken by: @dallasblissphotography"
+      />
+      <Card
+        title="A dog and his owner"
+        imageUrl={walking}
+        body="Stevie and her owner max walking through the woods Photo taken by: @dallasblissphotography"
+      />
+      <Card
+        title="Wild hike"
+        imageUrl={stevieRock}
+        body="Stevie posing on a rock so her best friend can take a picture of her Photo taken by: @dallasblissphotography"
+      />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./componentStyles/Card.css";
+
+
+export default function Card(props) {
+  const { title, imageUrl, body } = props;
+  console.log(imageUrl)
+  return (
+    <div className="card-container">
+      <div className="image-container">
+        <img src={imageUrl} alt="hehe" />
+      </div>
+      <div className="card-content">
+
+
+      <div className="card-title">
+            <h3>{title}</h3>
+      </div>
+      <div className="card-body">
+          <p>{body}</p>
+      </div>
+      </div>
+      <div className="card-button">
+            <button>
+                <a className="card-button-link">
+                    View More
+                </a>
+            </button>
+      </div>
+    </div>
+  );
+}
